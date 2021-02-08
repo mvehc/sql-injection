ba5535e51bd38693ae3b844a8c958c08f0619589
Jeffrey Meng <jeffkmeng@gmail.com>
2021-02-08 02:24:50 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
-import mysql from 'mysql';
+const crypto = require('crypto');
+const db = require('better-sqlite3')('db.sqlite3')
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.MVEHC_SQL_PASSWORD
+// remake the `users` table
+db.exec(`DROP TABLE IF EXISTS users;`);
+db.exec(`CREATE TABLE users(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  password TEXT
+);`);
+
+// add an admin user with a random password
+db.exec(`INSERT INTO users (username, password) VALUES (
+  'Admin',
+  '${crypto.randomBytes(16).toString('hex')}'
+)`);
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse json and serve static files
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('static'));
+
+// login route
+app.post('/login', (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        return res.redirect('/');
+    }
+
+
+    // see if user is in database
+    const query = `SELECT id FROM users WHERE
+    username = '${req.body.username}' AND
+    password = '${req.body.password}'
+  `;
+
+    let id;
+    try { id = db.prepare(query).get().id } catch {
+        return res.redirect('/');
+    }
+
+    // correct login
+    if (id) return res.sendFile('flag.html', { root: __dirname });
+
+    // incorrect login
+    return res.redirect('/');
 });
 
-db.connect(async function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    await db.query("CREATE DATABASE users");
-});
+app.listen(3000., () => console.log("Server is listening!"));
